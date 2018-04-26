03fe6a4280487a5a615fe2bbeb1667a8ac0b1cdf
Ryan Montgomery <mon16032@byui.edu>
2018-04-25 19:37:07 -0600

    added home page

---
+++ web/assignments.php
@@ -0,0 +1,17 @@
+<?php 
+    $ptitle='CS 313 - Assignments';
+    include('shared/_header.php');
+?>
+
+<main>
+    <h2 id="assignment" class="page-header">Assignments</h2>
+    
+    <!--<h3>Week 3</h3>
+    <a href="team/Week3/index.php">Teach - Team Activity</a><br>
+    <a href="assignments/week3/index.php">Prove - Simply Shopping Cart</a>-->
+    
+    <h3>Week 1</h3>
+    <a href="Assignments/Week-1/hello.html">Prove - Hello World</a>
+</main>
+
+<?php include('shared/_footer.php'); ?>
