73f5f296fd5fdb2296aa089e13e143ae66458ff5
Ryan Montgomery <mon16032@byui.edu>
2018-05-08 20:53:21 -0600

    Added teach 3 to assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -6,17 +6,14 @@
 <main>
     <h2 id="assignment" class="page-header">Assignments</h2>
     
-    <!--<h3>Week 3</h3>
-    <a href="team/Week3/index.php">Teach - Team Activity</a><br>
-    <a href="assignments/week3/index.php">Prove - Simply Shopping Cart</a>-->
-    
-    <h3>Week 1</h3>
-    <a href="Assignments/Week-1/hello.html">Prove - Hello World</a>
+    <h3>Week 3</h3>
+    <a href="Team/Week-3/index.php">Teach - 03</a>
 
     <h3>Week 2</h3>
     <a href="Team/Week-2/index.html">Teach - 02</a>
 
-
+    <h3>Week 1</h3>
+    <a href="Assignments/Week-1/hello.html">Prove - Hello World</a>
 </main>
 
 <?php include('shared/_footer.php'); ?>
