2a198920bf8075ba7cb082104bb13a1fe04277fb
Ryan Montgomery <mon16032@byui.edu>
2018-05-22 20:36:25 -0600

    add details page

--- web/Team/Week-5/details.php
+++ web/Team/Week-5/details.php
@@ -14,6 +14,27 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $db->prepare('SELECT * FROM scriptures.scriptures WHERE ');
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if(!empty($_GET['id'])){
+    $scripture_id = $_GET['id'];
+
+    $stmt = $db->prepare('SELECT * FROM scriptures.scriptures WHERE scripture_id = :scripture_id');
+    $stmt->bindValue(':scripture_id', $scripture_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<h1>Scripture Details</h1>';
+
+    foreach($rows as $row) {
+        echo '<p>';
+        echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong>';
+        echo '</p>';
+        echo '<p>';
+        echo $row['content'];
+        echo '</p>';
+}
+}
+
+else {
+    echo '<a href="index.php">Scripture not found!</a>';
+}
+
