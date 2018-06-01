6b4290d832af385df9386e8501c5159e3aa23618
Ryan Montgomery <mon16032@byui.edu>
2018-06-01 14:57:20 -0600

    Changed librarian dashboard link for form-post to anchor tag - get

--- web/Assignments/Week-6/view/librarian_dashboard.php
+++ web/Assignments/Week-6/view/librarian_dashboard.php
@@ -2,53 +2,23 @@
 include 'view/_header.php';
 ?>
 
-<div id="checkout" class="dashboard_section">
-    <h2>Check Out Books</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="check_out_books">
-        <input type="submit" value="Go">
-    </form>
-</div>
+<h2>Check Out Books</h2>
+<a href="index.php?action=check_out_books">Go</a>
 
-<div id="checkin" class="dashboard_section">
-    <h2>Check In Books</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="check_in_books">
-        <input type="submit" value="Go">    
-    </form>
-</div>
+<h2>Check In Books</h2>
+<a href="index.php?action=check_in_books">Go</a>
 
-<div id="patrons" class="dashboard_section">
-    <h2>Library Patrons</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="patron_dashboard">
-        <input type="submit" value="Go">
-    </form>
-</div>
+<h2>Library Patrons</h2>
+<a href="index.php?action=patron_dashboard">Go</a>
 
-<div id="authors" class="dashboard_section">
-    <h2>Authors</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="author_dashboard">
-        <input type="submit" value="Go">
-   </form>
-</div>
+<h2>Authors</h2>
+<a href="index.php?action=author_dashboard">Go</a>
 
-<div id="books" class="dashboard_section">
-    <h2>Books</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="book_dashboard">
-        <input type="submit" value="Go">
-   </form>
-</div>
+<h2>Books</h2>
+<a href="index.php?action=book_dashboard">Go</a>
 
-<div id="due_dates" class="dashboard_section">
-    <h2>Due Dates</h2>
-    <form action="index.php" method="post">
-        <input type="hidden" name="action" value="due_dates_dashboard">
-        <input type="submit" value="Go">
-   </form>
-</div>
+<h2>Due Dates</h2>
+<a href="index.php?action=due_dates_dashboard">Go</a>
 
 <?php
 include 'view/_footer.php';
